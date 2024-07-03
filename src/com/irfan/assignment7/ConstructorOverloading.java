7ab6437136a6fa2a086df65bb9a693fc1557e26f
Mohammed Irfan Siddiqui <mohammed.211751.cs@mhssce.ac.in>
2024-07-03 22:57:27 +0530

    assignment 7

---
+++ src/com/irfan/assignment7/ConstructorOverloading.java
@@ -0,0 +1,20 @@
+package com.irfan.assignment7;
+
+public class ConstructorOverloading {
+	
+	
+	
+	public ConstructorOverloading() {
+		System.out.println("constructor without parameter");
+	}
+	public ConstructorOverloading(int i) {
+		System.out.println("constructor with integer parameter");
+		// TODO Auto-generated constructor stub
+	}
+	public ConstructorOverloading(String string) {
+		// TODO Auto-generated constructor stub
+		System.out.println("constructor with string  parameter");
+	}
+	
+
+}
---
+++ src/com/irfan/assignment7/Main.java
@@ -0,0 +1,11 @@
+package com.irfan.assignment7;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Creating objects using different constructors
+		ConstructorOverloading cl = new ConstructorOverloading();
+		ConstructorOverloading c2 = new ConstructorOverloading(3);
+		ConstructorOverloading c3 = new ConstructorOverloading("irfan");
+	}
+}
